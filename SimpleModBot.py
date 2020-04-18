bcdb19112019e82241767d614bdaf416238b9fc1
Aditya <aditya26042005@gmail.com>
2020-04-18 15:21:21 -0700

    Capitalization changes.

--- SimpleModBot.py
+++ SimpleModBot.py
@@ -24,46 +24,58 @@ client = discord.Client(description=desc)
 
 @client.event
 async def on_message(message: discord.Message):
-    Channel = message.channel
+    channel = message.channel
     username = message.author.name
-    
+
     if (username == "ProfanityBot"):
         return
-    
-    if any([word in message.content.casefold() for word in config['words2']]):
+
+    if any([username in config['usernames']]):
         if (random.randint(1, 2) == 1):
-            await Channel.send('{} {}'.format(message.author.mention, config['response2']))
+            await channel.send('{} {}{}'.format("I am superior to", username, "."))
             return
         else:
-            await Channel.send('{} {}'.format(message.author.mention, config['response2.5']))
+            if (random.randint(1, 2) == 1):
+                await channel.send('{} {}{}'.format("I will take", username, "\'s toes and put 'em in my toe jar."))
+                return
+            else:
+                await channel.send('{} {}'.format(message.author.mention, "is stoopid."))
+                return
+
+    if any([word in message.content.casefold() for word in config['words2']]):
+        if (random.randint(1, 2) == 1):
+            await channel.send('{} {}'.format(message.author.mention, config['response2']))
+            return
+        else:
+            await channel.send('{} {}'.format(message.author.mention, config['response2.5']))
             return
 
     if any([word in message.content.casefold() for word in config['words']]):
         if (random.randint(1, 2) == 1):
-            await Channel.send('{} {}'.format(message.author.mention, config['response']))
+            await channel.send('{} {}'.format(message.author.mention, config['response']))
             return
         else:
-            await Channel.send('{}'.format("https://tenor.com/view/watch-your-profanity-funny-gif-5600117"))
+            await channel.send('{}'.format("https://tenor.com/view/watch-your-profanity-funny-gif-5600117"))
             return
 
     if any([word in message.content.casefold() for word in config['words3']]):
-        await Channel.send('{} {}'.format(message.author.mention, config['response3']))
+        await channel.send('{} {}'.format(message.author.mention, config['response3']))
         return
 
     if any([word in message.content.casefold() for word in config['words4']]):
-        await Channel.send('{} {}'.format(message.author.mention, config['response4']))
+        await channel.send('{} {}'.format(message.author.mention, config['response4']))
         return
 
     if any([word in message.content.casefold() for word in config['words5']]):
         if (random.randint(1, 2) == 1):
-            await Channel.send('{} {}'.format(config['response5'], message.author.mention))
+            await channel.send('{} {}'.format(config['response5'], message.author.mention))
             return
         else:
-            await Channel.send('{} {}'.format(config['response5.5'], message.author.mention))
+            await channel.send('{} {}'.format(config['response5.5'], message.author.mention))
             return
 
     if any([word in message.content.casefold() for word in config['words6']]):
-        await Channel.send('{}'.format(config['response6']))
+        await channel.send('{}'.format(config['response6']))
         return
 
     if any([word in message.content.casefold() for word in config['words7']]):
@@ -76,21 +88,21 @@ async def on_message(message: discord.Message):
                 Word = wordList[x]
                 randNum = random.randint(1, 3)
                 if (randNum == 1):
-                    await Channel.send('{} {}'.format(Word, config['response7']))
+                    await channel.send('{} {}'.format(Word, config['response7']))
                     return
                 elif (randNum == 2):
-                    await Channel.send('{}'.format("https://tenor.com/view/touchdown-bruh-really-gif-12484222"))
+                    await channel.send('{}'.format("https://tenor.com/view/touchdown-bruh-really-gif-12484222"))
                     return
                 else:
-                    await Channel.send('{}'.format("https://tenor.com/view/bruh-gif-5156041"))
+                    await channel.send('{}'.format("https://tenor.com/view/bruh-gif-5156041"))
                     return
 
     if any([word in message.content.casefold() for word in config['words8']]):
         if (random.randint(1, 2) == 1):
-            await Channel.send('{}'.format(config['response8']))
+            await channel.send('{}'.format(config['response8']))
             return
         else:
-            await Channel.send('{}'.format("https://tenor.com/view/what-do-you-mean-what-really-whatever-gif-12124162"))
+            await channel.send('{}'.format("https://tenor.com/view/what-do-you-mean-what-really-whatever-gif-12124162"))
             return
 
     if any([word in message.content.casefold() for word in config['words9']]):
@@ -101,29 +113,17 @@ async def on_message(message: discord.Message):
         for x in range(0, lengthOfList):
             if (wordPosBool[x] == True):
                 Word = wordList[x]
-                await Channel.send('{} {} {} {}'.format(message.author.mention, config['response9'], Word, config['response9.5']))
+                await channel.send('{} {} {} {}'.format(message.author.mention, config['response9'], Word, config['response9.5']))
                 return
 
     if any([word in message.content.casefold() for word in config['words10']]):
-        await Channel.send('{}'.format("https://tenor.com/view/whyareyougay-uganda-gay-gif-14399349"))
+        await channel.send('{}'.format("https://tenor.com/view/whyareyougay-uganda-gay-gif-14399349"))
         return
 
     if any([word in message.content.casefold() for word in config['words11']]):
-        await Channel.send('{}'.format("https://media.tenor.com/images/05de333b038141f6b8208c7ce8f8613c/tenor.gif"))
+        await channel.send('{}'.format("https://media.tenor.com/images/05de333b038141f6b8208c7ce8f8613c/tenor.gif"))
         return
 
-    username = message.author.name
-    if any([username in config['usernames']]):
-        if (random.randint(1, 2) == 1):
-            await Channel.send('{} {}{}'.format("I am superior to", username, "."))
-            return
-        else: 
-            if (random.randint(1, 2) == 1):
-                await Channel.send('{} {}{}'.format("I will take", username, "\'s toes and put 'em in my toe jar."))
-                return
-            else:
-                await Channel.send('{} {}'.format(message.author.mention, "is stoopid."))
-                return
 
 @client.event
 async def on_ready():
@@ -136,7 +136,7 @@ async def on_ready():
     perms.administrator = True
     url = discord.utils.oauth_url(app_info.id, perms)
     print('To invite me to a server, use this link\n{}'.format(url))
-    activity = discord.Game(name="with a broken back")
+    activity = discord.Game(name="consensually with MostestDankest's toes.")
     await client.change_presence(status=discord.Status.online, activity=activity)
 
 
