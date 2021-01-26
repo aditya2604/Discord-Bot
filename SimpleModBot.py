8629c38b489e5892972caa9390895ecf1d303263
“[Aditya <aditya26042005@gmail.com>
2021-01-25 18:26:05 -0800

    commented speak command

--- SimpleModBot.py
+++ SimpleModBot.py
@@ -63,9 +63,9 @@ async def leave(ctx):
     await ctx.voice_client.disconnect()
 
 # speak command
-@bot.command(brief="text to speech", description="speaks out the entered argument")
-async def speak(ctx, *, arg):
-    await ctx.send(arg, tts=True)
+# @bot.command(brief="text to speech", description="speaks out the entered argument")
+# async def speak(ctx, *, arg):
+#     await ctx.send(arg, tts=True)
 
 emojis = ['🤡', '😐', '😳', '🧢', '🏳️‍🌈', '💩', '😈', '🤓', '👲']
 
