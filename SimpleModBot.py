bd18fa90aaa9125cafbf94875476dc15d954fb12
“[Aditya <aditya26042005@gmail.com>
2021-02-02 22:53:01 -0800

    final testing for color randomizer

--- SimpleModBot.py
+++ SimpleModBot.py
@@ -31,10 +31,10 @@ bot = commands.Bot(command_prefix=',', help_command=None)
 # help command
 @bot.command(brief="shows this message", description="shows this message")
 async def help(ctx):
-    # colors = [0x4ef207, 0x6f5df0, 0x40ffcf, 0xa640ff, 0xe00d6c, 0xb2e835]
-    # _color = random.colors()
+    colors = [0x4ef207, 0x6f5df0, 0x40ffcf, 0xa640ff, 0xe00d6c, 0xb2e835]
+    _color = random.choice(colors)
     embed = discord.Embed(
-        title="Kermit's commands", url="https://en.wikipedia.org/wiki/Kermit_the_Frog", color=0x40ffcf
+        title="Kermit's commands", url="https://en.wikipedia.org/wiki/Kermit_the_Frog", color=_color
     )
     embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
     for command in bot.commands:
