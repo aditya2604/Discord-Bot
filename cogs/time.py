989562dcf95034fa2213a3be5939327406211df6
“[Aditya <aditya26042005@gmail.com>
2021-02-14 01:47:31 -0800

    added uptime command

---
+++ cogs/time.py
@@ -0,0 +1,27 @@
+import discord, datetime
+from discord.ext import commands
+
+start_time = datetime.datetime.utcnow()
+
+class time(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+
+    @commands.command(pass_context=True)
+    async def uptime(self, ctx):
+        current_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+        tdelta = str((datetime.datetime.strptime(current_time,'%H:%M:%S') - datetime.datetime.strptime(self.start_time,'%H:%M:%S')))
+        i = tdelta.find(':')
+        tdelta = tdelta.replace(':', " hours, ", 1)
+        tdelta = tdelta.replace(':', " minutes, ", 1)
+        tdelta += " seconds"
+        embed = discord.Embed(color=discord.Color.dark_gold())
+        embed.add_field(name="uptime", value=tdelta, inline=True)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(time(bot))
