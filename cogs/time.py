bf5dd26091a7ae6352af9e6df0c234b238d34fb6
“[Aditya <aditya26042005@gmail.com>
2021-02-14 16:11:48 -0800

    added absolute value

--- cogs/time.py
+++ cogs/time.py
@@ -14,7 +14,7 @@ class time(commands.Cog):
     @commands.command(description="displays the amount of time Kermit has been online for")
     async def uptime(self, ctx):
         current_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-        tdelta = str((datetime.datetime.strptime(current_time,'%H:%M:%S') - datetime.datetime.strptime(self.start_time,'%H:%M:%S')))
+        tdelta = str(abs((datetime.datetime.strptime(current_time,'%H:%M:%S') - datetime.datetime.strptime(self.start_time,'%H:%M:%S'))))
         i = tdelta.find(':')
         tdelta = tdelta.replace(':', " hours, ", 1)
         tdelta = tdelta.replace(':', " minutes, ", 1)
