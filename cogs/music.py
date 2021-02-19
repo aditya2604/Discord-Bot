613f48f0ea692d7e00a369bba7c1c2e59df38659
“[Aditya <aditya26042005@gmail.com>
2021-02-19 14:30:49 -0800

    bounty trial

--- cogs/music.py
+++ cogs/music.py
@@ -24,8 +24,8 @@ ytdl_format_options = {
 }
 
 ffmpeg_options = {
-    'options': '-vn',
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+    'options': '-vn'
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
