91db6660fd1b5435921f9b86c02488952b48ccc0
“[Aditya <aditya26042005@gmail.com>
2021-03-01 17:06:34 -0800

    wednesday meme sender

---
+++ cogs/wednesday.py
@@ -0,0 +1,26 @@
+import discord, datetime
+from discord.ext import commands, tasks
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
+
+class wednesday(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @tasks.loop(seconds=3600)
+    async def time_checker(self):
+        self.time = datetime.datetime.now
+        if self.time().hour == 18:
+            if datetime.datetime.today().weekday() == 0:
+                self.channel = await self.bot.fetch_channel(config['blue'])
+                await channel.send(file=discord.File('images/wednesday_pic.png'))
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.wait_until_ready()
+        self.time_checker.start()
+
+def setup(bot):
+    bot.add_cog(wednesday(bot))
