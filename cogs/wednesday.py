dbd579902e7cd75e5c0194beb3eefef4ff1a7cef
“[Aditya <aditya26042005@gmail.com>
2021-03-09 22:28:54 -0800

    commented wed cog

--- cogs/wednesday.py
+++ cogs/wednesday.py
@@ -1,26 +1,26 @@
-# import discord, datetime
-# from discord.ext import commands, tasks
-# import json
+import discord, datetime
+from discord.ext import commands, tasks
+import json
 
-# with open('config.json') as f:
-#     config = json.load(f)
+with open('config.json') as f:
+    config = json.load(f)
 
-# class wednesday(commands.Cog):
-#     def __init__(self, bot):
-#         self.bot = bot
+class wednesday(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
     
-#     @tasks.loop(hours=1)
-#     async def time_checker(self):
-#         self.channel = await self.bot.fetch_channel(config['g'])
-#         self.time = datetime.datetime.now
-#         if self.time().hour == 6:
-#             if datetime.datetime.today().weekday() == 2:
-#                 await self.channel.send(file=discord.File('images/wednesday_pic.png'))
+    # @tasks.loop(hours=1)
+    # async def time_checker(self):
+    #     self.channel = await self.bot.fetch_channel(config['g'])
+    #     self.time = datetime.datetime.now
+    #     if self.time().hour == 6:
+    #         if datetime.datetime.today().weekday() == 2:
+    #             await self.channel.send(file=discord.File('images/wednesday_pic.png'))
     
-#     @commands.Cog.listener()
-#     async def on_ready(self):
-#         await self.bot.wait_until_ready()
-#         self.time_checker.start()
+    # @commands.Cog.listener()
+    # async def on_ready(self):
+    #     await self.bot.wait_until_ready()
+    #     self.time_checker.start()
 
-# def setup(bot):
-#     bot.add_cog(wednesday(bot))
+def setup(bot):
+    bot.add_cog(wednesday(bot))
