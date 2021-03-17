a9a9630a90e16f2846c736788306196855fb20ba
“[Aditya <aditya26042005@gmail.com>
2021-03-16 20:30:43 -0700

    wednesday cog

--- cogs/wednesday.py
+++ cogs/wednesday.py
@@ -9,18 +9,18 @@ class wednesday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    # @tasks.loop(hours=1)
-    # async def time_checker(self):
-    #     self.channel = await self.bot.fetch_channel(config['g'])
-    #     self.time = datetime.datetime.now
-    #     if self.time().hour == 6:
-    #         if datetime.datetime.today().weekday() == 2:
-    #             await self.channel.send(file=discord.File('images/wednesday_pic.png'))
+    @tasks.loop(hours=1)
+    async def time_checker(self):
+        self.channel = await self.bot.fetch_channel(config['g'])
+        self.time = datetime.datetime.now
+        if self.time().hour == 3:
+            if datetime.datetime.today().weekday() == 1:
+                await self.channel.send(file=discord.File('images/wednesday_pic.png'))
     
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-    #     await self.bot.wait_until_ready()
-    #     self.time_checker.start()
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.wait_until_ready()
+        self.time_checker.start()
 
 def setup(bot):
     bot.add_cog(wednesday(bot))
