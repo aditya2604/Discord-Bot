3af407dfd72720e24efe9089b6bcd8e86e226e1a
“[Aditya <aditya26042005@gmail.com>
2021-03-16 21:27:08 -0700

    wednesday cog

--- cogs/wednesday.py
+++ cogs/wednesday.py
@@ -11,11 +11,15 @@ class wednesday(commands.Cog):
     
     @tasks.loop(hours=1)
     async def time_checker(self):
-        self.channel = await self.bot.fetch_channel(config['blue'])
+        self.channel = await self.bot.fetch_channel(config['g'])
+        self.channel_1 = await self.bot.fetch_channel(config['fg'])
+        self.channel_2 = await self.bot.fetch_channel(config['ag'])
         self.time = datetime.datetime.now
         if self.time().hour == 12:
             if datetime.datetime.today().weekday() == 3:
                 await self.channel.send(file=discord.File('images/wednesday_pic.png'))
+                await self.channel_1.send(file=discord.File('images/wednesday_pic.png'))
+                await self.channel_2.send(file=discord.File('images/wednesday_pic.png'))
     
     @commands.Cog.listener()
     async def on_ready(self):
