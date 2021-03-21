1789ed2c9492b678b66e9e3fcff47dbb8448bab0
“[Aditya <aditya26042005@gmail.com>
2021-03-20 19:58:07 -0700

    meme cog

--- cogs/meme.py
+++ cogs/meme.py
@@ -12,9 +12,20 @@ class meme(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(description="sends meme")
+    @commands.command(description="sends meme from r/memes")
     async def meme(self, ctx):
         subreddit = reddit.subreddit('memes')
+        posts = subreddit.hot(limit=50)
+        posts = list(posts)
+        post = random.choice(posts)
+        embed = discord.Embed(title=f"{post.title}", url=f"https://www.reddit.com{post.permalink})", description="", color=discord.Color.green())
+        embed.set_image(url=post.url)
+        embed.set_footer(text=f"👍 {post.score} | 💬 {post.num_comments}")
+        await ctx.send(embed=embed)
+    
+    @commands.command(description="sends meme from r/facepalm")
+    async def smh(self, ctx):
+        subreddit = reddit.subreddit('facepalm')
         posts = subreddit.hot(limit=50)
         posts = list(posts)
         post = random.choice(posts)
