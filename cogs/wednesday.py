291b99dafe250f0afad017c4efc6469e938ce967
Aditya Tomar <aditya26042005@gmail.com>
2021-04-03 23:52:28 -0700

    wednesday cog

--- cogs/wednesday.py
+++ cogs/wednesday.py
@@ -9,18 +9,21 @@ with open('config.json') as f:
 class Wednesday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    @tasks.loop(hours=1)
-    async def time_checker(self):
         self.channel = await self.bot.fetch_channel(config['g'])
         self.channel_1 = await self.bot.fetch_channel(config['fg'])
         self.channel_2 = await self.bot.fetch_channel(config['ag'])
+        self.channel_3 = await self.bot.fetch_channel(config['blue'])
+        self.time = datetime.datetime.now
+    
+    @tasks.loop(hours=1)
+    async def time_checker(self):
         self.time = datetime.datetime.now
         if self.time().hour == 12:
             if datetime.datetime.today().weekday() == 2:
                 await self.channel.send(file=discord.File('media/wednesday_pic.png'))
                 await self.channel_1.send(file=discord.File('media/wednesday_pic.png'))
                 await self.channel_2.send(file=discord.File('media/wednesday_pic.png'))
+                await self.channel_3.send(file=discord.File('media/wednesday_pic.png'))
     
     @commands.Cog.listener()
     async def on_ready(self):
